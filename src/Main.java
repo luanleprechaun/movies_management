cd322287ea9a24663f099ee6d387caff6a5317d9
João Vitor <joaov.lmsousa@gmail.com>
2024-08-28 19:21:06 -0300

    add método mergeSort

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         Filme[] filmes = new FilmeAux().gerarFilmes(10);
@@ -9,9 +7,17 @@ public class Main {
             System.out.println(filme.toString());
         }
 
-        System.out.println();
-        Arrays.sort(filmes);
+        // System.out.println();
+        // Arrays.sort(filmes.clone());
 
+        // System.out.println("Arrays sort");
+        // for (Filme filme : filmes) {
+        //     System.out.println(filme.toString());
+        // }
+        
+        System.out.println();
+        new Ordenacao().mergeSort(filmes);
+        System.out.println("Merge sort");
         for (Filme filme : filmes) {
             System.out.println(filme.toString());
         }
